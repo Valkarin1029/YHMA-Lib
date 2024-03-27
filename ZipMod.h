425c97d8f7ae5c6899e5b190887ce5c484370dbe
Valkarin <lucerooliver1@gmail.com>
2024-03-27 00:34:16 -0700

    Add project files.

---
+++ ZipMod.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <string>
+#include <zip.h>
+
+#include <Godot.hpp>
+#include <Node.hpp>
+
+using namespace std;
+namespace godot {
+
+class ZipMod : public Node
+{
+private:
+	GODOT_CLASS(ZipMod, Node)
+
+public:
+	static void _register_methods();
+
+	ZipMod();
+	~ZipMod();
+
+	void _init();
+	static bool is_dir(const std::string& dir);
+	static bool walk_dir(const std::string& startdir, const std::string& inputdir, zip_t* zipper);
+	void zipDirectory(String _input_dir, String _output_dir);
+
+};
+
+}
+
+
