d96151a0f9ce375f3c98928f8a9b83b928150250
Valkarin <lucerooliver1@gmail.com>
2024-04-08 13:57:16 -0700

    Removed unnecesary include and replace with std::filesystem.

--- ZipMod.cpp
+++ ZipMod.cpp
@@ -4,15 +4,15 @@
 #include <format>
 #include <filesystem>
 
-#include <dirent.h>
-#include <sys/stat.h>
 #include <zip.h>
 
 #include <Godot.hpp>
 #include <Node.hpp>
 
+
 using namespace std;
 using namespace godot;
+namespace fs = std::filesystem;
 
 void ZipMod::_register_methods()
 {
@@ -34,53 +34,42 @@ void ZipMod::_init()
 
 bool ZipMod::is_dir(const std::string& dir) 
 {
-	return std::filesystem::is_directory(dir);
+	return fs::is_directory(dir);
 }
 
-bool ZipMod::walk_dir(const std::string& startdir, const std::string& inputdir, zip_t* zipper)
+bool godot::ZipMod::walk_dir(const std::string& startdir, const std::string& inputdir, zip_t* zipper)
 {
-
 	std::string mod_folder = startdir.substr(startdir.rfind("/") + 1, startdir.length() - startdir.rfind("/"));
 
-	DIR* dp = ::opendir(inputdir.c_str());
-	if (dp == nullptr) {
-		Godot::print_error(std::format("[YHMA] Failed to open input directory: {}", std::string(::strerror(errno))).c_str(),
-			"ZipMod::walk_dir", "ZipMod.cpp", 45);
-		return false;
-	}
+	for (auto const& dir_entry : fs::recursive_directory_iterator(inputdir)) {
+		std::string path = dir_entry.path().string();
 
-	struct dirent* dirp;
-	while ((dirp = readdir(dp)) != NULL) {
-		if (dirp->d_name != std::string(".") && dirp->d_name != std::string("..")) {
-			std::string fullname = inputdir + "/" + dirp->d_name;
-			if (is_dir(fullname)) {
-				if (zip_dir_add(zipper, std::string(mod_folder + "/" + fullname.substr(startdir.length() + 1)).c_str(), ZIP_FL_ENC_UTF_8) < 0) {
-					//throw std::runtime_error("[YHMA] Failed to add subdirectory to zip: " + std::string(zip_strerror(zipper)));
-					Godot::print_error("[YHMA] Failed to add subdirectory to zip",
-						"ZipMod::walk_dir", "ZipMod.cpp", 56);
-					return false;
-				}
-				walk_dir(startdir, fullname, zipper);
-			}
-			else {
-				zip_source_t* source = zip_source_file(zipper, fullname.c_str(), 0, 0);
-				if (source == nullptr) {
-					//throw std::runtime_error("[YHMA] Failed to add file to zip: " + std::string(zip_strerror(zipper)));
-					Godot::print_error(std::format("[YHMA] Failed to add file to zip: {}", std::string(zip_strerror(zipper))).c_str(),
-						"ZipMod::walk_dir", "ZipMod.cpp", 66);
-					return false;
-				}
-				if (zip_file_add(zipper, std::string(mod_folder + "/" + fullname.substr(startdir.length() + 1)).c_str(), source, ZIP_FL_ENC_UTF_8) < 0) {
-					zip_source_free(source);
-					//throw std::runtime_error("[YHMA] Failed to add file to zip: " + std::string(zip_strerror(zipper)));
-					Godot::print_error(std::format("[YHMA] Failed to add file to zip: {}", std::string(zip_strerror(zipper))).c_str(),
-						"ZipMod::walk_dir", "ZipMod.cpp", 73);
-					return false;
-				}
+		if (dir_entry.is_directory()) {
+			if (zip_dir_add(zipper, std::string(mod_folder + "/" + path.substr(startdir.length() + 1)).c_str(), ZIP_FL_ENC_UTF_8) < 0) {
+				//throw std::runtime_error("[YHMA] Failed to add subdirectory to zip: " + std::string(zip_strerror(zipper)));
+				Godot::print_error("[YHMA] Failed to add subdirectory to zip",
+					"ZipMod::walk_dir", "ZipMod.cpp", 56);
+				return false;
 			}
 		}
+		else {
+			zip_source_t* source = zip_source_file(zipper, path.c_str(), 0, 0);
+			if (source == nullptr) {
+				//throw std::runtime_error("[YHMA] Failed to add file to zip: " + std::string(zip_strerror(zipper)));
+				Godot::print_error(std::format("[YHMA] Failed to add file to zip: {}", std::string(zip_strerror(zipper))).c_str(),
+					"ZipMod::walk_dir", "ZipMod.cpp", 66);
+				return false;
+			}
+			if (zip_file_add(zipper, std::string(mod_folder + "/" + path.substr(inputdir.length() + 1)).c_str(), source, ZIP_FL_ENC_UTF_8) < 0) {
+				zip_source_free(source);
+				//throw std::runtime_error("[YHMA] Failed to add file to zip: " + std::string(zip_strerror(zipper)));
+				Godot::print_error(std::format("[YHMA] Failed to add file to zip: {}", std::string(zip_strerror(zipper))).c_str(),
+					"ZipMod::walk_dir", "ZipMod.cpp", 73);
+				return false;
+			}
+		}
+
 	}
-	::closedir(dp);
 	return true;
 }
 
